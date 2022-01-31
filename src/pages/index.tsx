d3da4d619041b67b3d95981faf72c14f20efb779
wdrik <w.drik@outlook.com>
2022-01-31 12:12:22 -0300

    feat: add geolocation call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [location, setLocation] = useState<any>();
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported.');
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position: any) => {
+        const { latitude, longitude } = position.coords;
+
+        console.log(`latitude: `, latitude);
+        console.log(`longitude: `, longitude);
+      },
+      (error: any) => {
+        console.log(`error: `, error);
+      }
+    );
+  }, []);
+
   return (
     <>
       <main>
