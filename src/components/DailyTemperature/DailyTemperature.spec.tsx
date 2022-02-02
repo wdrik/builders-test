b0905ce7f8e8ec52f5adc4014321738f6532f26a
wdrik <w.drik@outlook.com>
2022-02-02 12:58:24 -0300

    test: add daily component tests

--- src/components/DailyTemperature/DailyTemperature.spec.tsx
+++ src/components/DailyTemperature/DailyTemperature.spec.tsx
@@ -1,9 +1,63 @@
 import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import DailyTemperature from '.';
+import theme from '../../styles/theme';
 
-describe('Teste Component', () => {
-  it('renders correctly', () => {
-    render(<h1>teste</h1>);
+type IWeather = {
+  icon: string;
+  day: number;
+  min: number;
+  max: number;
+};
 
-    expect(screen.getByText('teste')).toBeInTheDocument();
+interface IDailyTemperatureProps {
+  active?: boolean;
+  weather: IWeather;
+}
+
+const mook = {
+  weather: {
+    icon: '10bn',
+    day: 1643817380,
+    min: 20,
+    max: 30,
+  },
+};
+
+describe('DailyTemperature Component', () => {
+  it('renders correctly min temperature', () => {
+    const { debug } = render(
+      <ThemeProvider theme={theme}>
+        <DailyTemperature {...mook} />
+      </ThemeProvider>
+    );
+
+    debug();
+
+    expect(screen.getByText('20°')).toBeInTheDocument();
+  });
+
+  it('renders correctly max temperature', () => {
+    const { debug } = render(
+      <ThemeProvider theme={theme}>
+        <DailyTemperature {...mook} />
+      </ThemeProvider>
+    );
+
+    debug();
+
+    expect(screen.getByText('30° -')).toBeInTheDocument();
+  });
+
+  it('renders correctly day', () => {
+    const { debug } = render(
+      <ThemeProvider theme={theme}>
+        <DailyTemperature {...mook} />
+      </ThemeProvider>
+    );
+
+    debug();
+
+    expect(screen.getByText('Qua.')).toBeInTheDocument();
   });
 });
